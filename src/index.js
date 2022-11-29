4ec12967b81bfc85e5fc9edaa658e158e00905f9
PRANSHU SINGH <pranshusingh12@gmail.com>
2022-11-29 18:34:56 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
 import App from "./App";
-import ColorModeSwitcher from "../../project3/src/ColorModeSwitcher";
+import ColorModeSwitcher from "./ColorModeSwitcher";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
